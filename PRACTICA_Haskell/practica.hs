7ee9ed98857f4b9e9059e05a9d443203d53d11e8
Jerónimo Ledesma <jeronimoledesma2015@gmail.com>
2026-03-23 09:06:51 -0500

    Fst

---
+++ PRACTICA_Haskell/practica.hs
@@ -0,0 +1,128 @@
+import Text.Printf(printf)
+import Control.Monad.RWS (MonadState(put))
+type Student =  (String, Int, Maybe Int)--(id, in, out)
+
+-- checkIn
+checkIn :: [Student] -> IO [Student]
+checkIn lista = do
+    putStrLn "--Ingresando al campus--"
+    putStrLn "ID del estudiante: "
+    studentId <- getLine
+
+    let alreadyIn = any (\(id,_,salida) -> id == studentId && salida == Nothing) lista
+    if alreadyIn then do
+        putStrLn "El ID ya esta registrado"
+        return lista
+    else do
+        putStrLn "Hora de entrada (HH:MM): "
+        horaInStr <- getLine
+        let horaIn = aMin horaInStr
+        let new = (studentId, horaIn, Nothing)
+        putStrLn "Entrada registrada"
+
+        return (new : lista)
+-- checkOut
+checkOut :: [Student] -> IO [Student]
+checkOut lista = do
+    putStrLn "--Saliendo del campus--"
+    putStrLn "ID del estudiante: "
+    studentId <- getLine
+
+    let inCampus = filter (\(id,_,salida) -> id == studentId && salida == Nothing) lista
+    case inCampus of
+        [] -> do
+            putStrLn "El ID no está en el campus o no existe"
+            return lista
+        ((id, entrada, _):_) -> do
+            putStrLn "Hora de salida (HH:MM): "
+            horaOutStr <- getLine
+            let horaOut = aMin horaOutStr
+                nuevaLista = map (\(id, entrada, salida) ->
+                    if id == studentId
+                        then (id, entrada, Just horaOut)
+                        else (id, entrada, salida)
+                    ) lista
+            putStrLn "Se registró la salida"
+            return nuevaLista
+
+-- conversion de tiempo hora -> min
+aMin :: String -> Int
+aMin input =
+    let (hString, mString) = span (/=':') input
+        horas = read hString :: Int
+        minutos = read (tail mString) :: Int
+    in horas * 60 + minutos
+
+-- conversion de tiempo min -> hora
+aHour :: Int -> String
+aHour mins =
+    let h = mins `div` 60
+        m = mins `mod` 60
+    in printf "%02d:%02d" h m
+
+-- calculo de tiempo in
+tiempoIn :: Student -> String
+tiempoIn (id, entrada, salida) =
+    case salida of
+        Nothing -> "El estudiante sigue dentro del campus"
+        Just x ->
+            let total = x-entrada
+            in "Duración: " ++ aHour total ++ " horas"
+
+--filtrado/busqueda
+buscar :: [Student] -> IO()
+buscar lista = do
+    putStrLn "ID del estudiante: "
+    busquedaId <- getLine
+    let result = filter (\(id,_,_) -> id == busquedaId) lista
+    case result of
+        [] -> putStrLn "ID no encontrado"
+        ((id, entrada, salida):_) -> do
+            putStrLn $ "ID: " ++ id
+            putStrLn $ "Hora de entrada: " ++ aHour entrada
+            case salida of
+                Nothing -> putStrLn "El estudiante sigue dentro del campus"
+                Just x -> do
+                    putStrLn $ "Salida: " ++ aHour x
+                    putStrLn $ tiempoIn (id, entrada, salida)
+--mostrar todos los estudiantes
+mostrar :: [Student] -> IO()
+mostrar lista = do
+    mapM_ (\(id, entrada, salida)->do
+        putStrLn "Estudiantes registrados:"
+        putStrLn $ "Estudiante:  " ++ id
+        putStrLn $ "Hora de entrada: " ++ aHour entrada
+        case salida of
+            Nothing -> putStrLn "El estudiante sigue dentro del campus"
+            Just x -> putStrLn $ "Hora de salida: " ++ aHour x
+        putStrLn ""
+        ) lista
+main :: IO ()
+main = loop []
+    where
+        loop :: [Student] -> IO ()
+        loop lista = do
+            putStrLn "Seleccione una opción:"
+            putStrLn "1. Registrar entrada"
+            putStrLn "2. Registrar salida"
+            putStrLn "3. Buscar estudiante"
+            putStrLn "4. Listar estudiantes"
+            putStrLn "5. Salir"
+            option <- getLine
+            case option of
+                "1" -> do
+                    newList <- checkIn lista
+                    loop newList
+                "2" -> do
+                    newList <- checkOut lista
+                    loop newList
+                "3" -> do
+                    buscar lista
+                    loop lista
+                "4" -> do
+                    mostrar lista
+                    loop lista
+                "5" -> putStrLn "Saliendo..."
+                _   -> do
+                    putStrLn "Opción no válida, intente de nuevo."
+                    loop lista
---
+++ PRACTICA_Prolog/proyecto (1).pl
@@ -0,0 +1,120 @@
+:- dynamic estudiante/3.  % -- dynamic le dice a prolog que el predicado estudiante va a cambiar, asi que este permite usar assertz y retract 
+
+
+%-- imprimir en formato hh:mm --
+imprimir_hhmm(Total) :-
+    Horas is Total // 60,
+    Mins is Total mod 60,
+    write(Horas), write(':'),
+    (Mins < 10 -> write('0'), write(Mins) ; write(Mins)).
+
+% -- para calcular la duracion en el campus --
+duracion(ID, Duracion) :-
+    estudiante(ID, Entrada, Salida),
+    Salida =\= 0,
+    Duracion is Salida - Entrada.   
+
+%-- imprimir estudiante --
+imprimir_estudiante(estudiante(ID, Entrada, Salida)) :-
+        write('ID: '), write(ID),
+        write(' | Entrada: '), imprimir_hhmm(Entrada),
+        (Salida =:= 0 ->
+            write(' | Se encuentra en el campus'),nl
+        ;
+        Duracion is Salida - Entrada,
+            write(' | Salida: '), imprimir_hhmm(Salida),
+            write(' | Duracion: '), imprimir_hhmm(Duracion), write(' h'), nl
+        ), nl.
+
+
+% -- check in -- 
+check_in :- 
+    write('ID del estudiante: '), read(ID), 
+    (estudiante(ID, _, 0) ->
+        write('El estudiante sigue en el campus.'), nl
+    ;
+        write('Hora de entrada - Horas: '), read(Horas),
+        write('Hora de entrada - Minutos: '), read(Minutos),
+        Entrada is Horas * 60 + Minutos,      % -- para que se guarde en minutos en la bd y se puedan hacer operaciones matematicas
+        assertz(estudiante(ID, Entrada, 0)), 
+        write('Entrada registrada.'), nl,
+        guardar
+    ).
+    
+
+% -- buscar --
+buscar :-
+    write('ID del estudiante: '), read(ID),
+    (estudiante(ID, Entrada, Salida) ->
+        (Salida =:= 0 ->
+            write('El estudiante esta en el campus. Entrada: '), imprimir_hhmm(Entrada), nl
+        ;
+            duracion(ID, Duracion),
+            write('El estudiante ha salido. Salida: '), imprimir_hhmm(Salida),
+            write(' | Duracion: '), imprimir_hhmm(Duracion), write(' h'), nl
+        )
+    ;
+        write('Estudiante no encontrado.'), nl
+    ).
+
+% -- check out --
+check_out :-
+    write('ID del estudiante: '), read(ID),
+    write('Hora de Salida - Horas: '), read(Horas),
+    write('Hora de Salida - Minutos: '), read(Minutos),
+    Salida is Horas * 60 + Minutos,
+    (estudiante(ID, Entrada, 0) ->
+        retract(estudiante(ID, Entrada, 0)),
+        assertz(estudiante(ID, Entrada, Salida)),
+        write('Salida registrada.'), nl,
+        guardar
+    ;
+        write('No hay salida registrada.'), nl
+    ).
+
+
+% -- to list --
+listar :-
+    findall(estudiante(ID, Entrada, Salida), estudiante(ID, Entrada, Salida), Lista),
+    write('Estudiantes registrados:'), nl,
+    maplist(imprimir_estudiante, Lista).  % recorre la Lista y por cada estudiante llama a la funcion impr
+   
+
+%-- to save--
+guardar:-
+    open('University.txt', write, File),  % File es como el lapicero para escribir en el archivo
+    forall(
+        estudiante(ID, Entrada, Salida),
+        (write( File, estudiante(ID, Entrada, Salida) ), write(File, '.'), nl(File)) % con el File escribes DENTRO del archivo, sin el escribiria en pantalla 
+        ),
+        close(File).
+
+
+%--to load--
+cargar :-
+    (exists_file('University.txt') -> consult('University.txt') ; true).
+
+
+%--menu--
+menu :-
+    write('--- Menu ---'), nl,
+    write('1. Registrar entrada'), nl,
+    write('2. Buscar estudiante'), nl,
+    write('3. Registrar salida'), nl,
+    write('4. Listar estudiantes'), nl,
+    write('0. Salir'), nl,
+    write('Seleccione una opcion: '), read(Opcion), ejecutar(Opcion).
+
+    ejecutar(1) :- check_in, menu.  % para que se vuelva a ejecutar menu 
+    ejecutar(2) :- buscar, menu.
+    ejecutar(3) :- check_out, menu.
+    ejecutar(4) :- listar, menu.
+    ejecutar(0) :- write('Saliendo...'), nl.
+    ejecutar(_) :- write('Opcion no valida.'), nl, menu.
+
+inicio :-
+    cargar,
+    menu.
+
+:- inicio.
+
